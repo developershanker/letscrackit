03f25bc4dd6c9911ac4e595c02fc1cbd5dfa1957
Bhawana Shanker <bhawana.s@apollo247.org>
2024-01-21 21:32:24 +0530

    vector-icon lib added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 import { View, Text, StyleSheet } from 'react-native'
 import { colors, fonts } from "../utils/constants";
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Header = () => {
     return (
         <View>
             <Text style={styles.headerStyles}>Header</Text>
+            <Icon name="rss" size={30} color="#fff" />
         </View>
     )
 }

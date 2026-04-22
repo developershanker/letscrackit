5b4a742698112040760436cf49399e954c2878a6
Bhawana Shanker <bhawana.s@apollo247.org>
2026-04-22 21:21:17 +0530

    delete entire files.. because its outdated

--- src/store/rootReducer.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import userReducer from './slices/userSlice';
-
-const rootReducer = combineReducers({
-  user: userReducer,
-});
-
-export type RootState = ReturnType<typeof rootReducer>;
-export default rootReducer;

ad22145823d36286598b335515bc7cbe09c2cc0c
Bhawana Shanker <bhawana.s@apollo247.org>
2026-04-27 13:48:05 +0530

    commit few more automatic changes

---
+++ android/app/.cxx/RelWithDebInfo/2o5w1p2x/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/ReactAndroid/ReactAndroidConfig.cmake
@@ -0,0 +1,27 @@
+if(NOT TARGET ReactAndroid::hermestooling)
+add_library(ReactAndroid::hermestooling SHARED IMPORTED)
+set_target_properties(ReactAndroid::hermestooling PROPERTIES
+    IMPORTED_LOCATION "/Users/bhawanashanker/.gradle/caches/9.3.1/transforms/6e644eab27a879a8c611ecf37b859228/transformed/react-android-0.85.2-release/prefab/modules/hermestooling/libs/android.x86_64/libhermestooling.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/bhawanashanker/.gradle/caches/9.3.1/transforms/6e644eab27a879a8c611ecf37b859228/transformed/react-android-0.85.2-release/prefab/modules/hermestooling/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
+if(NOT TARGET ReactAndroid::jsi)
+add_library(ReactAndroid::jsi SHARED IMPORTED)
+set_target_properties(ReactAndroid::jsi PROPERTIES
+    IMPORTED_LOCATION "/Users/bhawanashanker/.gradle/caches/9.3.1/transforms/6e644eab27a879a8c611ecf37b859228/transformed/react-android-0.85.2-release/prefab/modules/jsi/libs/android.x86_64/libjsi.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/bhawanashanker/.gradle/caches/9.3.1/transforms/6e644eab27a879a8c611ecf37b859228/transformed/react-android-0.85.2-release/prefab/modules/jsi/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
+if(NOT TARGET ReactAndroid::reactnative)
+add_library(ReactAndroid::reactnative SHARED IMPORTED)
+set_target_properties(ReactAndroid::reactnative PROPERTIES
+    IMPORTED_LOCATION "/Users/bhawanashanker/.gradle/caches/9.3.1/transforms/6e644eab27a879a8c611ecf37b859228/transformed/react-android-0.85.2-release/prefab/modules/reactnative/libs/android.x86_64/libreactnative.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/bhawanashanker/.gradle/caches/9.3.1/transforms/6e644eab27a879a8c611ecf37b859228/transformed/react-android-0.85.2-release/prefab/modules/reactnative/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
